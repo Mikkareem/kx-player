fa8cd7bfe44b8b3750d5c351b32c094fd9f82f78
Mikkareem <mohdirsath1997@gmail.com>
2022-01-14 19:12:24 -0800

    dagger-hilt-setup

---
+++ app/src/main/java/dev/techullurgy/kxplayer/KXPlayerApplication.kt
@@ -0,0 +1,7 @@
+package dev.techullurgy.kxplayer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KXPlayerApplication: Application()
--- app/src/main/java/dev/techullurgy/kxplayer/MainActivity.kt
+++ app/src/main/java/dev/techullurgy/kxplayer/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import dagger.hilt.android.AndroidEntryPoint
 import dev.techullurgy.kxplayer.ui.theme.KXPlayerTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
