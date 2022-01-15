495f679cdeb1d73ac31c0c7e521f7bb3a6887854
IrsathKareem <IrsathKareem@inmach1859.sftin.com>
2022-01-14 17:30:50 -0800

    first commit

---
+++ app/src/main/java/dev/techullurgy/kxplayer/MainActivity.kt
@@ -0,0 +1,22 @@
+package dev.techullurgy.kxplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import dev.techullurgy.kxplayer.ui.theme.KXPlayerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KXPlayerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
